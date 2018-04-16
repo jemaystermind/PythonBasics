857e7cdc1dd5e08e96351b3d4a5fa02c7189fa19
Jeremy Tecson <jemay.mastermind@gmail.com>
2018-04-16 11:56:40 +0800

    (⌐■_■)

---
+++ mindfuck.py
@@ -0,0 +1,9 @@
+def add(x, y=10):
+    return x + y
+
+print("name: ", add.__code__.co_name)
+print("module: ", add.__module__)
+print("defaults: ", add.__defaults__)
+print("bytecode: ", add.__code__.co_code)
+print("local vars count: ", add.__code__.co_nlocals)
+print("var names: ", add.__code__.co_varnames)
