ad02a40ad27f88a2aadac8aa88b38b0d4323e997
Jeremy Tecson <jemay.mastermind@gmail.com>
2018-04-16 13:07:31 +0800

    Print source code

--- mindfuck.py
+++ mindfuck.py
@@ -1,5 +1,9 @@
+from inspect import getsource
+
+
 def add(x, y=10):
     return x + y
+
 
 print("name: ", add.__code__.co_name)
 print("module: ", add.__module__)
@@ -7,3 +11,7 @@ print("defaults: ", add.__defaults__)
 print("bytecode: ", add.__code__.co_code)
 print("local vars count: ", add.__code__.co_nlocals)
 print("var names: ", add.__code__.co_varnames)
+
+print("")
+
+print(getsource(add))
