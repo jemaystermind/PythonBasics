83fb4ad607242e1dd687a4865a43e47412930aba
Jeremy Tecson <jemay.mastermind@gmail.com>
2018-04-17 13:55:52 +0800

    Sample math functions

---
+++ calcs.py
@@ -0,0 +1,27 @@
+import math
+
+print("ceil(4.4) = ", math.ceil(4.4))
+print("floor(4.4) = ", math.floor(4.4))
+print("fabs(4.4) = ", math.fabs(-4.4))
+
+print("factorial(4) = ", math.factorial(4))
+
+print("fmod(5, 4) = ", math.fmod(5, 4))
+
+print("trunc(4.2) = ", math.trunc(4.2))
+
+print("pow(2, 2) = ", math.pow(2, 2))
+
+print("sqrt(4) = ", math.sqrt(4))
+
+print("math.e", math.e)
+print("math.pi", math.pi)
+
+print("exp(4)", math.exp(4))
+
+print("log(20)", math.log(20))
+print("log(100, 10)", math.log(100, 10))
+print("log10(1000)", math.log10(1000))
+
+print("degrees(1.5708)", math.degrees(1.5708))
+print("radians(90)", math.radians(90))
