be19e1449b74162618d547869d5dd5458545ef8f
Jeremy Tecson <jemay.mastermind@gmail.com>
2018-04-21 16:20:21 +0800

    Shookt!

--- temp.py
+++ temp.py
@@ -1,3 +1,11 @@
 import random
+import math
 
 print(random.randrange(1, 10))
+
+numbers = [i * 2 for i in range(10)]
+
+numbers_2 = [math.pow(i, 2) for i in range(20)]
+
+print(numbers)
+print(numbers_2)
