e591bb8ade1f4f216ed8af7c335621ec6d5bb968
Jeremy Tecson <jemay.mastermind@gmail.com>
2018-04-29 07:38:37 +0800

    Custom exception

---
+++ exception/error.py
@@ -0,0 +1,15 @@
+# Custom exception
+class DogNameError(Exception):
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+try:
+    dog_name = input("Enter dog name: ")
+
+    if any(char.isdigit() for char in dog_name):
+        raise DogNameError
+
+except DogNameError:
+    print("Dog name can't contain a number")
