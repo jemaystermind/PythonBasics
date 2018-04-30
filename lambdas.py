cac20f16eba21776d035018fa298b865e13d8599
Jeremy Tecson <jemay.mastermind@gmail.com>
2018-04-30 20:02:54 +0800

    Another lambda example

--- lambdas.py
+++ lambdas.py
@@ -2,3 +2,8 @@
 add = lambda x, y: x + y
 
 print("4 + 5 = ", add(4, 5))
+
+can_vote = lambda age: True if age >= 18 else False
+
+print("18 can vote?", can_vote(18))
+print("8 can vote?", can_vote(8))
